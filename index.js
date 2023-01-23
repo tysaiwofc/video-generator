5f346b8e4de2a9315d7e2765df67c81510ec5b35
nãoexistobb <87986195+naoexistobb@users.noreply.github.com>
2023-01-23 09:51:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const generateVideo = require('./bots/video.js')
-const uploader = require('./bots/poster.js')
 const { config } = require('dotenv')
+const { WEBHOOK_URL, WEBHOOK_URL2 } = require('./env.json')
+const questions = require('questions');
+const { WebhookClient } = require('discord.js')
+
+
+
 config()
 
 const teste = async () => {
-    await generateVideo('O Que é ' + 'Pão de queijo')
-    //await uploader('jujubasuporte@gmail.com', '2004135MARS2022', 'o pão frances é', 'acesse o discord.gg/jujuba')
-    //console.log('🔴 >>> GG, VIDEO FOI GERADO E POSTADO COM SUCESSO!')
+    questions.askOne({ info:'Qual vai ser o tema?' }, async function(result){
+        await generateVideo('O Que é ' + result)
+    })
 }
 
 teste()
+
